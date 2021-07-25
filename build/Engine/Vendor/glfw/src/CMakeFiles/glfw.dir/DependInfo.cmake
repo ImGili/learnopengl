cb547af4719039212fe224acb72e912f01acf385
xhguleixin123 <18017158729@163.com>
2021-07-25 18:25:03 +0800

    modified programm

---
+++ build/Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/cocoa_init.m" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/cocoa_joystick.m" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/cocoa_monitor.m" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/cocoa_time.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_time.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_time.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/cocoa_window.m" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/context.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/context.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/context.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/egl_context.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/init.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/init.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/init.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/input.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/input.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/input.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/monitor.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/monitor.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/monitor.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/nsgl_context.m" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/osmesa_context.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/posix_thread.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/vulkan.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o.d"
+  "/Users/gulei/Desktop/learnsomething/learnopengl/Engine/Vendor/glfw/src/window.c" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/window.c.o" "gcc" "Engine/Vendor/glfw/src/CMakeFiles/glfw.dir/window.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
