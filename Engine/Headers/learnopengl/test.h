6585c224ef6978cfdcef8638af35f98ee0410df7
xhguleixin123 <18017158729@163.com>
2021-09-27 11:07:14 +0800

    🌈 style: 删除点注释

--- Engine/Headers/learnopengl/test.h
+++ Engine/Headers/learnopengl/test.h
@@ -417,15 +417,6 @@ namespace glxtest2
         SkyBox skybox;
         FrameBufferObject fbo;
 
-        // Shader ourShader("./14/l1/ObjectVertex.vert", "./14/l1/ObjectFragment.frag");
-        Shader *ourShader = new Shader("./14/l1/ObjectVertex.vert", "./14/l1/ObjectFragment.frag");
-        ourShader->use();
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));     // it's a bit too big for our scene, so scale it down
-        ourShader->setMat4("model", model);
-        Model *ourModel = new Model("./models/nanosuit/nanosuit.obj");
-
         Object *obj = new Object(glm::vec3(1, 1, 1));
 
         mGUI::Init();
@@ -454,17 +445,10 @@ namespace glxtest2
 
             skybox.Draw();
             mmodel->Draw();
-            // mscene.Draw();
             // cubes
-            cube->Draw();
+            // cube->Draw();
             cube->DrawObject(obj);
-            // ourShader->use();
-            // glm::mat4 view = camera->GetViewMatrix();
-            // ourShader->setMat4("view", view);
-            // glm::mat4 projection = camera->getPerspective();
-            // ourShader->setMat4("projection", projection);
-
-            // ourModel->Draw(*ourShader);
+            
 
             // floor
             plane.Draw();
