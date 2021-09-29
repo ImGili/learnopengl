24fb36fff5a3c5701ba5830d5e3a7d5774098112
xhguleixin123 <18017158729@163.com>
2021-09-29 16:56:33 +0800

    ✨ feat: 利用高级数据缓存读取顶点数据测试全部通过

--- Engine/Headers/learnopengl/21-advanced-GLSL.h
+++ Engine/Headers/learnopengl/21-advanced-GLSL.h
@@ -26,7 +26,8 @@ namespace glx0211
         //     0.0f, 0.5f, 0.0f    // top
         // };
         // Drawable *drawable = (new Drawable())->SetDrawTypes(DrawTypes::POINTS)->SetVertexNum(3)->SetVertex(&vertices, Vertexlayout, sizeof(vertices))->SetShader("./21/l1/ObjectVertex.vert", "./21/l1/ObjectFragment.frag")->SetModle(glm::mat4(1));
-        Drawable *drawable = (new Drawable())->SetDrawTypes(DrawTypes::POINTS)->SetVertexNum(4)->SetVertexFromData("./data/21/l1/")->SetShader("./21/l1/ObjectVertex.vert", "./21/l1/ObjectFragment.frag")->SetModle(glm::mat4(1));
+        // Drawable *drawable = (new Drawable())->SetDrawTypes(DrawTypes::POINTS)->SetVertexNum(4)->SetVertexFromData("./data/21/l1/")->SetShader("./21/l1/ObjectVertex.vert", "./21/l1/ObjectFragment.frag")->SetModle(glm::mat4(1));
+        Drawable *drawable = (new Drawable())->SetDrawTypes(DrawTypes::POINTS)->SetVertexLayout(VertexNormallayout)->SetVertexNum(4)->SetVertexFromData("./data/21/l1/")->SetShader("./21/l1/ObjectVertex.vert", "./21/l1/ObjectFragment.frag")->SetModle(glm::mat4(1));
 
         Plane plane;
         SkyBox skybox;
