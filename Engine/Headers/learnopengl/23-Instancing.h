ede8ac01cd2f6fb8f3957e63eb85d5a62cb910f9
xhguleixin123 <18017158729@163.com>
2021-10-04 23:35:18 +0800

    🧪 test: 添加一个测试

--- Engine/Headers/learnopengl/23-Instancing.h
+++ Engine/Headers/learnopengl/23-Instancing.h
@@ -152,3 +152,84 @@ namespace glx0232
     }
 
 }
+
+// 使用实例化
+namespace glx0233
+{
+    int main()
+    {
+        Window *window = Window::getWindow();
+        CameraInstance *camera = CameraInstance::GetCamera();
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+        glPointSize(200);
+        
+        Drawable* drawable =    (new Drawable())\
+                                ->SetVertexNum(6)\
+                                ->SetDrawInstance(100)\
+                                ->SetVertexLayout(VertexNormallayout)\
+                                ->SetVertexFromData("./data/23/l1/")\
+                                ->SetShader("./23/l2/ObjectVertex.vert", "./23/l2/ObjectFragment.frag")\
+                                ->SetModle(glm::mat4(1));
+        glm::vec3 translations[100];
+        int index = 0;
+        float offset = 0.1f;
+        for(int y = -10; y < 10; y += 2)
+        {
+            for(int x = -10; x < 10; x += 2)
+            {
+                glm::vec3 translation;
+                translation.x = (float)x / 10.0f + offset;
+                translation.y = (float)y / 10.0f + offset;
+                translation.z = 0;
+                translations[index++] = translation;
+            }
+        }
+        for(unsigned int i = 0; i < 100; i++)
+        {
+            stringstream ss;
+            string index;
+            ss << i; 
+            index = ss.str(); 
+            drawable->GetShader()->setVec3(("offsets[" + index + "]").c_str(), translations[i]);
+        }
+        
+        
+        mGUI::Init();
+        // render loop
+        // -----------
+        while (!window->WindowShouldClose())
+        {
+            window->UpdateDeltaTime();
+            camera->UpdateUniform();
+            // input
+            // -----
+            window->processInput();
+
+            
+            window->Clear();
+
+            mGUI::NewFrame();
+            {
+                ImGui::Begin(u8"实例化");
+                ImGui::Text("FPS:%f", 1/window->deltaTime);
+                ImGui::End();
+            }
+
+            mGUI::RenderGUI();
+
+            drawable->Draw();
+            
+
+            mGUI::DrawRenderData();
+            window->SwapBufferAndPollEvents();
+        }
+
+        mGUI::DestroyGUI();
+        Window::DestoryWindow();
+        CameraInstance::DestoryCamera();
+        return 0;
+    }
+
+}
