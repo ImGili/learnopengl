3245e8ed4ee2f27542281cfa8029a8d14d5ffa45
xhguleixin123 <18017158729@163.com>
2021-10-07 22:37:55 +0800

    🦄 refactor: 修改一下编译指令和清空文件内容

--- Engine/include/rw_config.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#define GET_SHADER_PATH(x) (std::string("/Users/gulei/Desktop/learnsomething/learnopengl/build/Engine/resources/shaders") + std::string(x)).c_str()
-#define GET_RESOURCE_PATH(x) (std::string("/Users/gulei/Desktop/learnsomething/learnopengl/build/Engine/resources/") + std::string(x)).c_str()
